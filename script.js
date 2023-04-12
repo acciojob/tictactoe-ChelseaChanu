e3cea4f9e86cfb2e4fe0347c9994ca0133622765
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-12 14:25:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const firstContainer = document.querySelector(".conatiner-1");
+const secondContainer = document.querySelector(".conatiner-2");
+const btn = document.getElementById("submit");
+const player1 = document.getElementsByTagName("input")[0].value;
+const player2 = document.getElementsByTagName("input")[1].value;
+const play = document.querySelector("label")[1];
+
+btn.addEventListener("click",()=>{
+	firstContainer.style.display = "none";
+	secondContainer.style.display = "block"
+});

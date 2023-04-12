24fa1ff1a61dabacab40d18ddd1599d68d74fd7f
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-12 18:03:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 //your JS code here. If required.
 const firstContainer = document.querySelector(".conatiner-1");
 const secondContainer = document.querySelector(".conatiner-2");
+const
 const btn = document.getElementById("submit");
-const player1 = document.getElementsByTagName("input")[0].value;
-const player2 = document.getElementsByTagName("input")[1].value;
-const meassage = document.querySelector(".meassage");
-	
-btn.addEventListener("click",()=>{
-	firstContainer.style.display = "none";
-	secondContainer.style.display = "block"
-	meassage.innerText = `${player1}, you're up`;
-});
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player1");
+const message = document.querySelector(".message");
+
+btn.addEventListener("click",(e)=>{
+    if((player1.value==="" && player2.value==="") || (player1.value!=="" && player2.value==="")
+	  || (player2.value!=="" && player1.value==="")){
+        e.preventDefault();
+    }
+	else{
+		firstContainer.style.display = "none";
+	    secondContainer.style.display = "block"
+        message.textContent = `${player1.value}, you're up`;
+	}
+}); 
+
